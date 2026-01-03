37079416521d796426e43c081af0b6341babc8f6
Débora Andrade <deboraellenandrade@gmail.com>
2026-01-03 16:50:29 +0000

    feat: Finaliza responsividade.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,30 @@
+/* --- ENVIO DO FORMULÁRIO (Simulação) --- */
 function enviarForm(event) {
-    event.preventDefault(); // Impede o refresh da página
+    event.preventDefault();
     alert("Transmissão recebida com sucesso! Câmbio, desligo. 🍄");
-    event.target.reset(); // Limpa o formulário
+    event.target.reset();
 }
+
+/* --- MENU HAMBÚRGUER --- */
+const burgerMenu = document.getElementById('burger-menu');
+const menuNav = document.getElementById('menu');
+const menuLinks = document.querySelectorAll('#menu a'); // Seleciona todos os links do menu
+
+// Função para alternar o menu (abrir/fechar)
+function toggleMenu() {
+    burgerMenu.classList.toggle("close");
+    menuNav.classList.toggle("overlay");
+}
+
+// Evento de clique no ícone do hambúrguer
+burgerMenu.addEventListener('click', toggleMenu);
+
+// Evento: Fechar o menu ao clicar num link (Melhor UX)
+menuLinks.forEach(link => {
+    link.addEventListener('click', () => {
+        // Se o menu estiver aberto, fecha-o
+        if (menuNav.classList.contains('overlay')) {
+            toggleMenu();
+        }
+    });
+});
